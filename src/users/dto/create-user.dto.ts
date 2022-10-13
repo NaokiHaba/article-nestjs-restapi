fd35b681cf7c9e03dd4b0a3ab8e94a21dd22524d
naoki-haba <haba@mic9.co.jp>
2022-10-13 22:14:24 +0900

    feat: âdd validate

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,8 @@
-export class CreateUserDto {}
+import { MaxLength } from 'class-validator';
+
+export class CreateUserDto {
+  @MaxLength(255, {
+    message: 'アカウント名は255文字以内で入力してください',
+  })
+  name: string;
+}
