e0eadb11aa556c332409519d36c30982d88fc213
naoki-haba <haba@mic9.co.jp>
2022-10-13 21:38:45 +0900

    feat: init typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'data/dev.sqlite',
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
